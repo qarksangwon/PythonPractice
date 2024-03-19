aa53d2dcd5a3e7ed81aba4dc963dd08aa1dad7bc
qarksangdol <0629psw@gmail.com>
2024-03-19 11:16:04 +0900

    자료형 타입

---
+++ 자료형.py
@@ -0,0 +1,20 @@
+# type() 데이터형 확인
+num = 10
+num2 = 3.14
+str1 = "123"
+bool1 = True
+print(type(num))
+print(type(num2))
+print(type(str1))
+print(type(bool1))
+
+print(10 + int("10"))
+print(str(10) + "10")
+print(0.1 * float("512.33"))
+
+val1 = "" # default 값은 false
+num3 = 0
+val2 = None
+print(bool(val1))
+print(bool(num3))
+print(bool(val2))
