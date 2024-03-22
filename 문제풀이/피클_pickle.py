fd7c753732d954b1a2a961ba813297ce0545f95f
qarksangdol <0629psw@gmail.com>
2024-03-22 10:29:40 +0900

    파일 입출력 , 피클

---
+++ 문제풀이/피클_pickle.py
@@ -0,0 +1,17 @@
+# 파이썬 피클(Pickle) 은 파이썬 객체를 직렬화(serialize) 하고 역직렬화(deserialize) 하는 모듈
+# 피클을 사용하면 파이썬 객체를 파일에 저장하거나 네트워크를 통해 전송할 수 있다.
+# 객체를 이진 형식 (binary) 으로 변환 저장해 필요 시 원래 상태로 복원 가능
+
+import pickle
+
+# 객체를 직렬화하여 파일에 저장하기
+data = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+with open('data.pickle', 'wb') as file:
+    pickle.dump(data, file)
+
+# 파일에서 객체를 역직렬화하여 복원하기
+with open('data.pickle', 'rb') as file:
+    restored_data = pickle.load(file)
+
+print(restored_data)  # {'name': 'Alice', 'age': 30, 'city': 'New York'}
+
---
+++ 파일입출력.py
@@ -0,0 +1,38 @@
+score_file = open("score.txt","w",encoding="utf-8") # write 모드는 새로 써짐
+# 기존 파일에 덧입력 하고싶다면, "a"
+# 한글은 유니코드 체제 -> utf-8
+
+# 내용 넣는 방식은 두가지
+print("수학 : 95",file=score_file)
+print("영어 : 50",file=score_file)
+score_file.write("과학 : 80\n")
+score_file.write("국어 : 100\n")
+score_file.close()
+
+# 파일은 항상 open -> close 거쳐야 한다. (with 키워드로 생략할 수 있음)
+
+# 파일을 썼으니 읽을 차례
+score_file = open("score.txt", "r", encoding="utf-8")
+
+# read() : 파일 내의 모든 내용을 읽어 하나의 문자열로 반환
+# readline() : 한 라인씩 읽어 문자열 반환, 파일의 끝(EOF) 에 도달 시 None => "" 반환
+# readlines() : 모든 라인을 순서대로 읽어 하나의 요소로 저장하는 리스트 반환
+
+# while True:
+#     line = score_file.readline()
+#     if not line : break
+#     print(line, end="")
+
+lines = score_file.readlines()
+print(lines)
+for line in lines:
+    print(line, end="")
+
+score_file.close()
+print()
+
+# with 사용 시 구문이 종료될 때 알아서 close 해줘서 반드시 쓰는걸 강추
+with open("score.txt", "r", encoding="utf-8") as score_file:
+    lines = score_file.readlines()
+    for line in lines:
+        print(line, end= "")
