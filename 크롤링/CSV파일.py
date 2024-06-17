8bac4b338594bd2c20633304ae67c5b5892f5187
qarksangdol <0629psw@gmail.com>
2024-06-17 09:51:21 +0900

    CSV reader 추가

--- 크롤링/CSV파일.py
+++ 크롤링/CSV파일.py
@@ -4,3 +4,8 @@ wr = csv.writer(f)
 wr.writerow([1, "박재훈", "클래식피지크", True])
 wr.writerow([2, "크리스범스테드", "클래식피지크", False])
 f.close()
+
+f2 = open('output.csv', 'r', encoding='utf-8')
+reader = csv.reader(f2)
+for row in reader:
+    print(row)
