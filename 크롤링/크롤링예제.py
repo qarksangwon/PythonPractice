81972e0d324e2aac4bea6d77fe1b6206aae84b92
qarksangdol <0629psw@gmail.com>
2024-06-17 11:09:57 +0900

    html5lib 사용해서 가져오기 추가

--- 크롤링/크롤링예제.py
+++ 크롤링/크롤링예제.py
@@ -16,12 +16,25 @@ html = '''
             <td> 300 </td>
         </tr>
     </table>
+    <ul>
+        <li> 100 </li> 
+        <li> 200 </li>
+    </ul> 
+    <ol>
+        <li> 300 </li> 
+        <li> 400 </li>
+    </ol>
 </html>        
 '''
 
 soup = BeautifulSoup(html, 'html.parser')
+# 사용 : pip install html5lib 설치 해야함
+soup2 = BeautifulSoup(html, 'html5lib')
 result = soup.select('td')
-print(result)
+result2 = soup2.select('ul li')
 
+print(result)
 for val in result:
     print(val.text)
+
+print(result2)
