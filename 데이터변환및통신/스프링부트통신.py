ad934a783770878d5649de37b888d579840f5545
qarksangdol <0629psw@gmail.com>
2024-06-17 10:31:36 +0900

    스프링부트 통신 수정

--- 데이터변환및통신/스프링부트통신.py
+++ 데이터변환및통신/스프링부트통신.py
@@ -7,11 +7,10 @@ data = {
 }
 
 url = "http://localhost:8111/login"
-headers = {"Content-Type":"application/json"}
-
+headers = {"Content-Type" : "application/json"}
 response = requests.post(url, data=json.dumps(data), headers = headers)
 
 if(response.status_code == 200):
-    print("데이터 전송 성공")
+    print("데이터 전송 성공 [", response, "]")
 else:
-    print("데이터 전송 실패 [", response.status_code,"]")
+    print("데이터 전송 실패 [", response.status_code, "]")
