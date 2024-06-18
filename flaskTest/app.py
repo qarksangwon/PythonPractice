0444dd992cdcd02ffda3b36e9ae7a601fc3d0222
qarksangdol <0629psw@gmail.com>
2024-06-18 11:17:14 +0900

    CORS 설정

--- flaskTest/app.py
+++ flaskTest/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.data import get_data
 from routes.weather import get_weather
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000'])
 
 app.add_url_rule('/api/data', 'get_data', get_data, methods=['GET'])
 app.add_url_rule('/api/weather', 'get_weather', get_weather, methods=['GET'])
