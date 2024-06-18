ee2cc7e993b814e789c6a39b2010dbf6e68af902
qarksangdol <0629psw@gmail.com>
2024-06-18 10:41:11 +0900

    get 방식 (path -> url에 변수)

--- flaskTest/app.py
+++ flaskTest/app.py
@@ -46,6 +46,15 @@ def get_query():
     output += f"<{item_type} style='color : {item_color}'>item_color</{item_type}>"
     return output
 
+@app.route('/api/item/<item_id>', methods=['GET'])
+def get_path_item(item_id):
+    output = ""
+    path_item = item_id
+    output += f"<h1>{path_item}</h1>"
+    return output
+
+
+
 # 서버 실행
 if __name__ == '__main__':
     app.run(debug=True)
